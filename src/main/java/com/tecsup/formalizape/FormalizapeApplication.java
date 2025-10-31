1dd54d03ec48b4f0753bfde540d9b82f2247f4e0
AlvaroJBH <alvarobuenoh@gmail.com>
2025-10-30 19:23:50 -0500

    build(core): initialize project structure

---
+++ src/main/java/com/tecsup/formalizape/FormalizapeApplication.java
@@ -0,0 +1,13 @@
+package com.tecsup.formalizape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormalizapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormalizapeApplication.class, args);
+    }
+
+}
